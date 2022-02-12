a38cb31547fe8331aa873bb282417b85672b624b
Elctrony <18064@stemegypt.edu.eg>
2022-02-12 17:04:13 +0200

    Modification

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,46 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(RootApp());
+
+class RootApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+          body: Column(
+        children: <Widget>[
+          Container(
+            height: 100,
+            color: Colors.white,
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.red,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.green,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.blue,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.orange,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.purple,
+            ),
+          ),
+        ],
+      )),
+    );
+  }
+}
