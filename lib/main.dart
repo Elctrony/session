b1625c332bd1a6bfdf5b62b3ebcc8edbdecc148b
Elctrony <18064@stemegypt.edu.eg>
2022-02-17 20:07:54 +0200

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,175 +1,88 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(RootApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+class RootApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
+      home: HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final imagePath = [
-    'assets/images/land1.jpg',
-    'assets/images/land2.jpg',
-    'assets/images/land3.jpg',
-    'assets/images/land4.jpg',
-    'assets/images/land5.jpg'
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List textList = [
+    'https://merriam-webster.com/assets/ld/word_of_the_day/images/2540/large.jpg',
+    'https://www.celebritycruises.com/blog/content/uploads/2020/12/famous-landmarks-in-europe-eiffel-tower-paris-france-683x1024.jpg',
+    'https://www.landmarkhealth.org/wp-content/uploads/2019/04/CA.jpg',
+    'https://tourflag.com/wp-content/uploads/2018/05/%D8%B3%D9%8A%D8%A7%D8%AD%D8%A9-%D8%A7%D9%84%D9%85%D8%A7%D9%86%D9%8A%D8%A7-1.jpg',
+    'https://wallpaperaccess.com/full/170249.jpg',
+    'https://eskipaper.com/images/landscape-wallpaper-hd-32.jpg'
   ];
+
+  int index = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          leading: IconButton(onPressed: () {}, icon: Icon(Icons.list)),
-          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.search))],
-          title: const Text('Landscape'),
-          backgroundColor: Colors.red),
-      body: BodyPage(),
-    );
-  }
-}
-
-class BodyPage extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return BodyPageState();
-  }
-}
-
-class BodyPageState extends State<BodyPage> {
-  final List<Map> image = [
-    {
-      'name': 'Land of Fire',
-      'path': 'assets/images/land1.jpg',
-      'description':
-          'In the land of Fire, heroes born with ability to manpiulate fire'
-    },
-    {
-      'name': 'Land of Wave',
-      'path': 'assets/images/land2.jpg',
-      'description':
-          'In the land of Fire, heroes born with ability to manpiulate fire'
-    },
-    {
-      'name': 'Land of Sand',
-      'path': 'assets/images/land3.jpg',
-      'description':
-          'In the land of Fire, heroes born with ability to manpiulate fire'
-    },
-    {
-      'name': 'Land of Water',
-      'path': 'assets/images/land4.jpg',
-      'description':
-          'In the land of Fire, heroes born with ability to manpiulate fire'
-    },
-    {
-      'name': 'Land of Cloud',
-      'path': 'assets/images/land5.jpg',
-      'description':
-          'In the land of Fire, heroes born with ability to manpiulate fire'
-    },
-  ];
-  int index = 0;
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(30),
-            child: Image.asset(
-              image[index]['path'],
-              fit: BoxFit.fill,
-            ),
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          Card(
-            color: Colors.red[700],
-            elevation: 5.0,
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Column(
-                children: [
-                  Text('${image[index]['name']}: ',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 26,
-                          fontWeight: FontWeight.w500)),
-                  Text(image[index]['description'],
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w300))
-                ],
-              ),
-            ),
-          ),
-          Expanded(child: SizedBox()),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        appBar: AppBar(
+          title: Text('IEEE'),
+        ),
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              SizedBox(
-                width: 120,
-                height: 45,
-                child: MaterialButton(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12)),
-                  color: Colors.red,
-                  onPressed: () {
-                    if (index == 0) return;
-
-                    setState(() {
-                      index = index - 1;
-                    });
-                  },
-                  child: const Text(
-                    'Back',
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
+              Container(
+                child: Image.network(
+                  textList[index],
+                  width: double.infinity,
+                  fit: BoxFit.fitWidth,
                 ),
               ),
-              SizedBox(
-                width: 120,
-                height: 45,
-                child: MaterialButton(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12)),
-                  color: Colors.red,
-                  onPressed: () {
-                    if (index == (image.length - 1)) return;
-                    setState(() {
-                      index = index + 1;
-                    });
-                  },
-                  child: const Text(
-                    'Next',
-                    style: TextStyle(color: Colors.white, fontSize: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  MaterialButton(
+                    onPressed: () {
+                      if (index == 0) return;
+                      setState(() {
+                        index--;
+                      });
+                    },
+                    child: Text(
+                      'Back',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    color: Colors.red,
                   ),
-                ),
+                  MaterialButton(
+                    onPressed: () {
+                      if (index == (textList.length - 1)) return;
+                      setState(() {
+                        index++;
+                      });
+                    },
+                    child: Text(
+                      'Next',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    color: Colors.red,
+                  ),
+                ],
               )
             ],
-          )
-        ],
-      ),
-    );
+          ),
+        ));
   }
 }
